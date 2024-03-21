51b0ce652a14bd2e9c89407cc2f13b98eab255a2
Jurre Brandsen <jurre.brandsen@infosupport.com>
2024-03-21 16:03:01 +0100

    entire green label is now clickable

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -91,7 +91,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className,
     )}
     {...props}
