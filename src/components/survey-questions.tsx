d5151dbab8a055c827271e9cbd6f6540d142c705
Jurre Brandsen <jurre.brandsen@infosupport.com>
2024-03-21 11:36:39 +0100

    Add descriptions to each answer option for desktop

--- src/components/survey-questions.tsx
+++ src/components/survey-questions.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/components/ui/hover-card";
+
+import {
   type Role,
   type AnswerOption,
   type Question,
@@ -19,7 +25,7 @@ import { useState } from "react";
 
 import { api } from "~/trpc/react";
 import { type Session } from "next-auth";
-import { idToTextMap } from "~/utils/optionMapping";
+import { idToMoreInfo, idToTextMap } from "~/utils/optionMapping";
 
 import { Button } from "~/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
@@ -41,6 +47,7 @@ import {
   onSubmit,
   useGenerateFormAndSchema,
 } from "~/utils/survey-utils";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 
 export function SurveyQuestions({
   session,
@@ -134,10 +141,26 @@ export function SurveyQuestions({
         <Table divClassname="">
           <TableHeader className="sticky top-0 z-10 h-10 w-full bg-slate-100 dark:bg-slate-900">
             <TableRow>
-              <TableHead className="w-[200px]">Question</TableHead>
+              <TableHead className="w-[400px]">Question</TableHead>
               {answerOptions.map((option) => (
-                <TableHead key={option.id}>
-                  {idToTextMap[option.option]}
+                <TableHead className="text-center" key={option.id}>
+                  <HoverCard>
+                    <HoverCardTrigger asChild>
+                      <Button variant="link" className="no-underline">
+                        {idToTextMap[option.option]}
+                        <InfoCircledIcon className="ml-2 h-4 w-4" />
+                      </Button>
+                    </HoverCardTrigger>
+                    <HoverCardContent className="w-80">
+                      <div className="flex justify-between space-x-4">
+                        <div className="space-y-1">
+                          <p className="text-sm font-normal">
+                            {idToMoreInfo[option.option]}
+                          </p>
+                        </div>
+                      </div>
+                    </HoverCardContent>
+                  </HoverCard>
                 </TableHead>
               ))}
             </TableRow>
@@ -165,7 +188,7 @@ export function SurveyQuestions({
                     {answerOptions.map((option) => (
                       <TableCell
                         key={option.id}
-                        className={`${field.value === option.id || responses[question.id] === option.id ? "bg-custom-selectedLight dark:bg-custom-selected rounded-lg" : ""}`}
+                        className={`${field.value === option.id || responses[question.id] === option.id ? "rounded-lg bg-custom-selectedLight dark:bg-custom-selected" : ""} w-[300px]`}
                       >
                         <FormItem>
                           <FormControl>
