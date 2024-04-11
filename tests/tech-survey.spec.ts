437c9120ec352728c4ea4ab9629037ec76373a4f
JurreBrandsenInfoSupport <149962077+JurreBrandsenInfoSupport@users.noreply.github.com>
2024-04-11 10:36:46 +0200

    72 e2e test with playwright (#89)
    * e2e tests
    
    * fix of typechceck error
    
    * forgot to remove unused env var

--- tests/tech-survey.spec.ts
+++ tests/tech-survey.spec.ts
@@ -15,12 +15,9 @@ const navigateAndCheckSurveyPage = async (
     selectedRoles = roles;
   } else {
     selectedRoles = (await landingPage.selectRandomRoles()).filter(
-
       (role): role is string => typeof role === "string",
     );
   }
-
-
   await landingPage.navigateToSurveyPage();
   await surveyPage.getCurrentURL();
   const sortedSelectedRoles =
