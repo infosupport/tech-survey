265a4ee6c0679cf81c0b00b3466d634b41b427bf
Jurre Brandsen <jurre.brandsen@infosupport.com>
2024-05-15 10:36:56 +0200

    tmp

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,32 +61,7 @@ export default defineConfig({
       },
       dependencies: ["setup"],
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  /* No webserver config, webserver is started within the tests */
 });
--- tests/landing-page.ts
+++ tests/landing-page.ts
@@ -4,38 +4,18 @@ import { type Page } from "playwright";
 
 export class LandingPage {
   private readonly page: Page;
+  private readonly port;
 
-  constructor(page: Page) {
+  constructor(page: Page, port: number) {
     this.page = page;
+    this.port = port;
   }
 
   async navigateToLandingPage() {
-    await this.page.goto("/");
+    await this.page.goto(`http://localhost:${this.port}/`);
     await expect(
       this.page.getByRole("heading", { name: "Select Roles" }),
     ).toBeVisible();
-  }
-
-  async selectRandomRoles() {
-    const roles = this.page.locator("input[type=checkbox]");
-    const elementsCount = await roles.count();
-    const texts = [];
-
-    // select n random roles
-    const randomRoles = Math.floor(Math.random() * elementsCount);
-    for (let i = 0; i < randomRoles; i++) {
-      const randomIndex = Math.floor(Math.random() * elementsCount);
-      const role = roles.nth(randomIndex);
-
-      // get the text of the role, which is under the input element as a label
-      const roleElement = await role.elementHandle();
-      const siblingElement = await roleElement?.$("+ *");
-      const innerText = await siblingElement?.innerText();
-
-      texts.push(innerText);
-      await role.check();
-    }
-    return texts;
   }
 
   async navigateToSurveyPage() {
