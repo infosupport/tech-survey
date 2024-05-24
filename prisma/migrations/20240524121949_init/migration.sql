6e489dc055a132a3bfcc3daf06d38686db0acede
JurreBrandsenInfoSupport <149962077+JurreBrandsenInfoSupport@users.noreply.github.com>
2024-05-24 14:33:29 +0200

    127 code clean up (#129)
    fixed the naming of clip-rule and fill-rule

--- prisma/migrations/20240524121949_init/migration.sql
+++ prisma/migrations/20240524121949_init/migration.sql
@@ -1,7 +1,11 @@
+-- CreateEnum
+CREATE TYPE "CommunicationMethod" AS ENUM ('SLACK', 'EMAIL', 'WHATSAPP', 'SIGNAL', 'PHONE', 'TEAMS');
+
 -- CreateTable
 CREATE TABLE "Survey" (
     "id" TEXT NOT NULL,
     "surveyName" TEXT NOT NULL,
+
     CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
 );
 
@@ -10,6 +14,7 @@ CREATE TABLE "Role" (
     "id" TEXT NOT NULL,
     "role" TEXT NOT NULL,
     "default" BOOLEAN NOT NULL DEFAULT false,
+
     CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
 );
 
@@ -18,6 +23,7 @@ CREATE TABLE "Question" (
     "id" TEXT NOT NULL,
     "surveyId" TEXT NOT NULL,
     "questionText" TEXT NOT NULL,
+
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
 );
 
@@ -25,6 +31,7 @@ CREATE TABLE "Question" (
 CREATE TABLE "AnswerOption" (
     "id" TEXT NOT NULL,
     "option" INTEGER NOT NULL,
+
     CONSTRAINT "AnswerOption_pkey" PRIMARY KEY ("id")
 );
 
@@ -34,7 +41,17 @@ CREATE TABLE "QuestionResult" (
     "userId" TEXT NOT NULL,
     "questionId" TEXT NOT NULL,
     "answerId" TEXT NOT NULL,
+
     CONSTRAINT "QuestionResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CommunicationPreference" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "methods" "CommunicationMethod"[] DEFAULT ARRAY[]::"CommunicationMethod"[],
+
+    CONSTRAINT "CommunicationPreference_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -44,6 +61,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
@@ -62,6 +80,7 @@ CREATE TABLE "Account" (
     "scope" TEXT,
     "id_token" TEXT,
     "session_state" TEXT,
+
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
 
@@ -71,6 +90,7 @@ CREATE TABLE "Session" (
     "sessionToken" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
+
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
@@ -124,61 +144,34 @@ CREATE UNIQUE INDEX "_QuestionToRole_AB_unique" ON "_QuestionToRole"("A", "B");
 CREATE INDEX "_QuestionToRole_B_index" ON "_QuestionToRole"("B");
 
 -- AddForeignKey
-ALTER TABLE
-    "Question"
-ADD
-    CONSTRAINT "Question_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Question" ADD CONSTRAINT "Question_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "QuestionResult"
-ADD
-    CONSTRAINT "QuestionResult_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "AnswerOption"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuestionResult" ADD CONSTRAINT "QuestionResult_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "AnswerOption"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "QuestionResult"
-ADD
-    CONSTRAINT "QuestionResult_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuestionResult" ADD CONSTRAINT "QuestionResult_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "QuestionResult"
-ADD
-    CONSTRAINT "QuestionResult_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuestionResult" ADD CONSTRAINT "QuestionResult_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "Account"
-ADD
-    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "CommunicationPreference" ADD CONSTRAINT "CommunicationPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "Session"
-ADD
-    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "_UserRole"
-ADD
-    CONSTRAINT "_UserRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "_UserRole"
-ADD
-    CONSTRAINT "_UserRole_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_UserRole" ADD CONSTRAINT "_UserRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "_QuestionToRole"
-ADD
-    CONSTRAINT "_QuestionToRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_UserRole" ADD CONSTRAINT "_UserRole_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE
-    "_QuestionToRole"
-ADD
-    CONSTRAINT "_QuestionToRole_B_fkey" FOREIGN KEY ("B") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_QuestionToRole" ADD CONSTRAINT "_QuestionToRole_A_fkey" FOREIGN KEY ("A") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_QuestionToRole" ADD CONSTRAINT "_QuestionToRole_B_fkey" FOREIGN KEY ("B") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
