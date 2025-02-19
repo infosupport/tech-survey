5e460e2ec63de7e5806f42dfd1c4cb994f5f3977
Jelle Buitenhuis <jelle.buitenhuis@infosupport.com>
2025-02-19 13:05:36 +0100

    Fix eslint error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,5 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as unknown;
 export { handler as GET, handler as POST };
