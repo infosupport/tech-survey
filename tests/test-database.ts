f5b675eb3ffb35446bd56f51f5dbd1f8d45d4994
Jelle Buitenhuis <jelle.buitenhuis@infosupport.com>
2025-03-07 14:46:00 +0100

    refactor playwright.yml to run database tests using tsx instead of playwright test

---
+++ tests/test-database.ts
@@ -0,0 +1,28 @@
+﻿import { DbHelper } from "./db-helper";
+import * as fs from "fs";
+import { TestSetup } from "./test-setup";
+import { PrismaClient } from "@prisma/client";
+
+async function testDatabase() {
+    const client = new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL,
+            },
+        },
+        log: [
+            {
+                emit: "event",
+                level: "query",
+            },
+        ],
+    });
+
+    const answers = await client.answerOption.findMany();
+    console.log("Answer options:", answers);
+}
+
+testDatabase().catch((error) => {
+    console.error("Error in test-database.ts:", error);
+    process.exit(1);
+});
