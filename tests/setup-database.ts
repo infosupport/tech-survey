eb51dad68887aa58514b69a429702c73158ad37c
Jelle Buitenhuis <jelle.buitenhuis@infosupport.com>
2025-03-07 12:14:41 +0100

    refactor setup-database to log database connection details and update DATABASE_URL format

--- tests/setup-database.ts
+++ tests/setup-database.ts
@@ -2,6 +2,11 @@
 import * as fs from "fs";
 
 const dbHelper = await DbHelper.create();
+console.log(dbHelper.getContainer().getDatabase());
+console.log(dbHelper.getContainer().getPort());
+console.log(dbHelper.getContainer().getHost());
+console.log(dbHelper.getContainer().getUsername());
+console.log(dbHelper.getContainer().getPassword());
 
 const githubEnvPath = process.env.GITHUB_ENV;
 if (!githubEnvPath) {
@@ -9,8 +14,14 @@ if (!githubEnvPath) {
     process.exit(1);
 }
 
+const username = dbHelper.getContainer().getUsername();
+const password = dbHelper.getContainer().getPassword();
+const host = "postgres";
+const port = dbHelper.getContainer().getPort();
+const database = dbHelper.getContainer().getDatabase();
+
 const envVars = {
-    DATABASE_URL: dbHelper.getContainer().getConnectionUri(),
+    DATABASE_URL: `postgresL//${username}:${password}@${host}:${port}/${database}`,
 };
 
 for (const [key, value] of Object.entries(envVars)) {
