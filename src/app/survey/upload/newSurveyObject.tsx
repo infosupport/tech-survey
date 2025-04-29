fd063429aa073679a0c9537eb4db11935d2fdf86
Jelle Buitenhuis <126448139+jellebuitenhuisis@users.noreply.github.com>
2025-04-29 11:09:13 +0200

    fix: rename default role for new surveys
    `default` was renamed to `isDefault`, but this was not caught in the earlier review, because that review was done before the change

---
+++ src/app/survey/upload/newSurveyObject.tsx
@@ -0,0 +1,18 @@
+﻿import { z } from "zod";
+
+export const newSurveyObject = z.object({
+    surveyDate: z.string().transform((val) => new Date(val)),
+    surveyName: z.string(),
+    questions: z.array(
+        z.object({
+            questionText: z.string(),
+            roles: z.array(
+                z.object({
+                    id: z.string(),
+                    role: z.string(),
+                    isDefault: z.boolean(),
+                }),
+            ),
+        }),
+    ),
+});
